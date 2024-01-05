045aaf0cd8200c81a9700c77442f23b8e8318b4a
Sergey Babchenkov <hotsalami@yandex.ru>
2024-01-05 20:53:44 +0300

    Add files via upload

---
+++ NewsPortal/NewsPortal/celery.py
@@ -0,0 +1,18 @@
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcdonalds.settings')
+
+app = Celery('news_portal')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+app.conf.beat_scheldule = {
+    'action_every_monday_8am': {
+        'task': 'news.tasks.weekly_send_email_task',
+        'scheldule': crontab(hour=8, minute=0, day_of_week='monday'),
+        'args': ()
+    }
+}
